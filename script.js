73d03b25b87fd8b6d32a0b65fbc60426b52876e0
inthemonzon <inthemonzon@yahoo.com>
2021-07-13 14:26:04 -0400

    Decent progress on the project!

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
+document.addEventListener("DOMContentLoaded", () => {
+    fetch ("http://acnhapi.com/v1/fish/")
+    .then (Response => Response.json())
+    .then (fishData => {
+        console.log(fishData)
+        debugger
+    }) 
+})
 
-document.querySelector('body').appendChild(h2); 
+const renderFish = (fishData) => {
+    let tableBody = document.getElementById('table-body')
+    let tableRow = document.createElement('tr')
+    tableRow.className = 'fish-name'
+
+    let tableDataName = document.createElement('td')
+    tableDataName.textContent = fishData.name
+
+    let tableDataTime = document.createElement('td')
+    tableDataTime.textContent = fish.time
+
+    let tableDataPrice = document.createElement('td')
+    tableDataPrice.textContent = fish.price
+
+    let tableDataShadow = document.createElement('td')
+    tableDataShadow.textContent = fish.shadow
+
+    let editButton = document.createElement('button')
+    editButton.textContent = 'Edit Fish'
+    editFish(editButton)
+
+    tableRow.append(tableDataName, tableDataTime, tableDataPrice, tableDataShadow, editButton)
+    tableBody.append(tableRow)
+}
+
+const editFish = (editButton) => {
+    editButton.addEventListener('click', (e) => {
+        document.querySelector('fish-form').elements.name.value
+        document.querySelector('fish-form').element
+        document.querySelector('fish-form').element
+    })
+}
+
+
+   
+  
