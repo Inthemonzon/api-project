7cb03ad82e490d6f066bcbde5b8366325a577050
inthemonzon <inthemonzon@yahoo.com>
2021-07-13 19:54:47 -0400

    Really impressed with what I've done

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
+    fetchFish(null)
+
+    document.getElementById("fishSearch").addEventListener('click', (e) => {
+        document.getElementById("table-body").innerHTML = ""
+        fetchFish(document.getElementById("fishName").value)
+    })
+})
+
+const fetchFish = (filter) => (
     fetch ("http://acnhapi.com/v1/fish/")
     .then (Response => Response.json())
     .then (fishData => {
-        console.log(fishData)
-        debugger
+        Object.keys(fishData).forEach( key => {
+            // console.log(key)
+            let name = fishData[key].name["name-USen"]
+            if (filter==null) {
+                renderFish(fishData[key])
+            } else if (name.includes(filter)) {
+                renderFish(fishData[key])
+            }  
+        }) 
     }) 
-})
+)
 
-const renderFish = (fishData) => {
+const renderFish = (fish) => {
     let tableBody = document.getElementById('table-body')
     let tableRow = document.createElement('tr')
     tableRow.className = 'fish-name'
 
     let tableDataName = document.createElement('td')
-    tableDataName.textContent = fishData.name
+    tableDataName.textContent = fish.name["name-USen"]
 
     let tableDataTime = document.createElement('td')
-    tableDataTime.textContent = fish.time
+    tableDataTime.textContent = fish.availability.time
 
     let tableDataPrice = document.createElement('td')
     tableDataPrice.textContent = fish.price
@@ -24,22 +40,17 @@ const renderFish = (fishData) => {
     let tableDataShadow = document.createElement('td')
     tableDataShadow.textContent = fish.shadow
 
-    let editButton = document.createElement('button')
-    editButton.textContent = 'Edit Fish'
-    editFish(editButton)
+    let locationButton = document.createElement('button')
+    locationButton.textContent = 'View Location'
+    let location = fish.availability.location
+    addLocationListener(locationButton, location)
 
-    tableRow.append(tableDataName, tableDataTime, tableDataPrice, tableDataShadow, editButton)
+    tableRow.append(tableDataName, tableDataTime, tableDataPrice, tableDataShadow, locationButton)
     tableBody.append(tableRow)
 }
 
-const editFish = (editButton) => {
-    editButton.addEventListener('click', (e) => {
-        document.querySelector('fish-form').elements.name.value
-        document.querySelector('fish-form').element
-        document.querySelector('fish-form').element
+const addLocationListener = (locationButton, location) => {
+    locationButton.addEventListener('click', (e) => {
+       window.alert(location)
     })
 }
-
-
-   
-  
